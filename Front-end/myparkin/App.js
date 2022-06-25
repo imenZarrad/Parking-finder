6871738350bd1a2eebc09f834cbe1feed880a199
Sarah Kcm <100924437+sarahkcm@users.noreply.github.com>
2022-06-25 12:54:14 +0200

    initializing UI kit and implementing three screens view (slider)

--- Front-end/myparkin/App.js
+++ Front-end/myparkin/App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigation from './User/StackNavigator';
+import { StatusBar } from 'expo-status-bar';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigation />
+      <StatusBar style='dark' />
+    </NavigationContainer>
   );
 }
 
