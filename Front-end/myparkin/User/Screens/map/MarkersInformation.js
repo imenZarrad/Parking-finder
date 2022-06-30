dc1cf42cc3876c81d3a9f4887bbfc36d444f9019
azizsayela <boussayelaaziz@gmail.com>
2022-06-30 18:07:35 +0100

    added a cancel button that reset the map size and close the parking detail card details button redirect to parking details and pass the object information as params

--- Front-end/myparkin/User/Screens/map/MarkersInformation.js
+++ Front-end/myparkin/User/Screens/map/MarkersInformation.js
@@ -5,44 +5,38 @@ const MarkersInformation = [
       latitude: 36.82177601988111,
       longitude: 10.189817954456263,
     },
-    parkingName: "Parking1",
-    description: "description of parking 1",
+    parkingName: "Parking elAlem",
+    adress: "Rue Jamel abdelNaceur Tounes ",
     image:
-      "https://lh5.googleusercontent.com/p/AF1QipPOBqOiSBpIhousS4TW_jK3C9qeRN7v_rWMy20=w500-h500-k-no",
+      "https://ijaa.tn/wp-content/uploads/2019/01/Parking-el-Alem-Mise-en-Avant.png",
     price: "2dt/hour",
+    number: "36 343 654",
   },
-  {
-    coordinate: {
-      latitude: 36.80405180968878,
-      longitude: 10.183209322208434,
-    },
-    parkingName: "Palmarium Parking",
-    description: "description of parking ",
-    image:
-      "https://lh5.googleusercontent.com/p/AF1QipPOBqOiSBpIhousS4TW_jK3C9qeRN7v_rWMy20=w500-h500-k-no",
-    price: "2dt/hour",
-  },
+
   {
     coordinate: {
       latitude: 36.80123556699369,
       longitude: 10.181034978614065,
     },
     parkingName: "Centrale Park parking",
-    description: "description of parking ",
+    adress: " Av. Jean Jaurès, Tunis ",
     image:
       "https://lh3.googleusercontent.com/p/AF1QipPcAC9WVOrSyX9DEXToVGWrFlAR2FFEfwJk4j9Y=w600-k",
     price: "2dt/hour",
+    number: "71 861 174",
   },
   {
     coordinate: {
       latitude: 36.7991903464347,
       longitude: 10.181034978614065,
     },
-    parkingName: "Parking Alem",
+    parkingName: "Parking Palmarium",
     description: "description of parking ",
     image:
-      "https://lh3.googleusercontent.com/p/AF1QipPcAC9WVOrSyX9DEXToVGWrFlAR2FFEfwJk4j9Y=w600-k",
+      "https://lh5.googleusercontent.com/p/AF1QipPOBqOiSBpIhousS4TW_jK3C9qeRN7v_rWMy20=w500-h500-k-no",
     price: "2dt/hour",
+    adress: "Avenue Habib Bourguiba, Tunis",
+    number: "71 336 344",
   },
 ];
 
