225e18fe64a0b4cf8fa867a03ffa1f69efd3984d
imenZarrad <imenezarrad@gmail.com>
2022-07-04 18:32:29 +0100

    Edit profile fonctionality with UserName and Email

--- Front-end/myparkin/User/StacKNav2.js
+++ Front-end/myparkin/User/StacKNav2.js
@@ -5,10 +5,7 @@ import MarkerDetail from "./Screens/MarkerDetail";
 import SearchFilter from "./Screens/SearchFilter";
 import HestoryParking from "./Screens/hertoryParking.js";
 
-
 import ParkingDetail from "./Screens/ParkingDetail/ParkingDetail";
-
-
 
 import Map from "./Screens/map/Map.js";
 import Profile from "./Screens/Profile.js";
@@ -17,20 +14,19 @@ import MyParkings from "./Screens/MyParkings.js";
 
 import EditProfile from "./Screens/EditProfile.js";
 import SelectVec from "./Screens/SelectVec";
-import test from "./Screens/test.js";
+// import test from "./Screens/test.js";
 
 const Stack = createNativeStackNavigator();
 
 const StackNav = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="tes" component={test} />
-      <Stack.Screen name="SelectVec" component={SelectVec} />
+      {/* <Stack.Screen name="tes" component={test} /> */}
+      {/* <Stack.Screen name="SelectVec" component={SelectVec} /> */}
 
       <Stack.Screen name="Map" component={Map} />
       <Stack.Screen name="MyBookMark" component={MyBookMark} />
       <Stack.Screen name="Profile" component={Profile} />
-
 
       <Stack.Screen name="HestoryParking" component={HestoryParking} />
       <Stack.Screen name="ProfileFill" component={ProfileFill} />
