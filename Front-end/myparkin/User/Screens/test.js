c5f1cfa86e079a20dc0447dd3126dfa8ac1ddd5b
medmahfoudhi <elmahfoudhi.med92@gmail.com>
2022-07-11 16:33:36 +0200

    med

--- Front-end/myparkin/User/Screens/test.js
+++ Front-end/myparkin/User/Screens/test.js
@@ -1,45 +1,89 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
-import { doc, setDoc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase.config";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Pressable,
+  Text,
+} from "react-native";
+import {
+  doc,
+  setDoc,
+  getDocs,
+  collection,
+  Firestore,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { database, db } from "../../firebase.config";
+import { child, push, ref } from "firebase/database";
+import { firebase } from "../../firebase.config";
+import { FlatList } from "react-native-gesture-handler";
 const test = () => {
-  const [obj, setObj] = useState({
-    type: "track",
-    carDetails: {
-      brand: "volvo",
-      model: "CX90",
-      color: "black",
-      carPlate: "123TU9087",
-    },
-    bookDetails: {
-      date: "12/12/2022",
-      start: "14:00",
-      end: "15:00",
-      price: "3",
-      place: "A12",
-    },
-  });
-  const [obj1, setobj1] = useState({});
-  const docRef = doc(db, "bookin", "book");
-  const docSnap = async () => {
-    setobj1(await getDoc(docRef));
+  const [bookings, setBookings] = useState([]);
+  const obj = {
+    CarType: "volw",
+    ParkingName: "srarr",
+    Adress: "aaa",
+    Floor: "",
+    ParkingSpot: "aa",
+    Date: "12",
+    Duration: "1",
   };
+  // const bookingsRef = firebase.firestore().collection("bookings");
+  useEffect(async () => {
+    const querySnapshot = await getDocs(collection(db, "bookings"));
+    // bookingsRef.onSnapshot((querySnapshot) => {
+    const bookings = [];
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", typeof doc.data());
+      // const {
+      const {
+        CarType,
+        ParkingName,
+        Adress,
+        Floor,
+        ParkingSpot,
+        Date,
+        Duration,
+      } = doc.data();
+      bookings.push({
+        id: doc.id,
+        CarType,
+        ParkingName,
+        Adress,
+        Floor,
+        ParkingSpot,
+        Date,
+        Duration,
+      });
+      setBookings(bookings.filter((e) => e.ParkingName === "aziz"));
+    });
+    // });
+    // const bookingsRef = firebase.
+    // const docSnap = getDoc(bookingsRef);
+  }, []);
 
-  const booking = () => {
-    setDoc(doc(db, "bookin", "book"), obj);
-  };
-  // function handleChange(text, eventName) {
-  //   setObj((prev) => {
-  //     return {
-  //       ...prev,
-  //       [eventName]: text,
-  //     };
-  //   });
-  // }
+  function postBookings() {
+    const newKey = push(child(ref(database), "bookings")).key;
+    setDoc(doc(db, "bookings", `${newKey}`), obj);
+    // console.log(newKey);
+  }
   return (
     <View style={styles.container}>
-      {console.log(obj1, "jjjj")}
-      <Button title="add" onPress={docSnap} />
+      {console.log("kkk", bookings[0], "jjjj")}
+      <Button title="add" onPress={postBookings} />
+      <FlatList
+        data={bookings}
+        numColumns={1}
+        renderItem={({ item }) => (
+          <Pressable>
+            <View>
+              <Text>{item.ParkingName}</Text>
+            </View>
+          </Pressable>
+        )}
+      />
     </View>
   );
 };
@@ -50,3 +94,22 @@ const styles = StyleSheet.create({
     top: 122,
   },
 });
+// const {
+//   CarType,
+//   ParkingName,
+//   Adress,
+//   Floor,
+//   ParkingSpot,
+//   Date,
+//   Duration,
+// } = doc.data();
+// bookings.push({
+//   CarType,
+//   ParkingName,
+//   Adress,
+//   Floor,
+//   ParkingSpot,
+//   Date,
+//   Duration,
+// });
+// setBookings(bookings);
--- Front-end/myparkin/User/StacKNav2.js
+++ Front-end/myparkin/User/StacKNav2.js
@@ -4,7 +4,7 @@ import ProfileFill from "./Screens/ProfileFill.js";
 import MarkerDetail from "./Screens/MarkerDetail";
 import SearchFilter from "./Screens/SearchFilter";
 import HestoryParking from "./Screens/hertoryParking.js";
-
+import Login from "./Screens/Login.js";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Parkingspot from "./Screens/Parkingspot";
@@ -20,18 +20,24 @@ import SelectVec from "./Screens/SelectVec";
 import test from "./Screens/test.js";
 import FillCarInformation from "./Screens/FillCarInformation.js";
 import ParkingSpot_1 from "./Screens/ParkingSpot_1.js";
-import ParkingSpot_2 from './Screens/ParkingSpot_2';
-import ParkingSpot_3 from './Screens/ParkingSpot_3';
-import ParkingTimer from './Screens/ParkingTimer';
-
+import ParkingSpot_2 from "./Screens/ParkingSpot_2";
+import ParkingSpot_3 from "./Screens/ParkingSpot_3";
+import ParkingTimer from "./Screens/ParkingTimer";
+import SuccessfullyCreated from "./Screens/SuccessfullyCreated.js";
 const Stack = createNativeStackNavigator();
 
 const StackNav = () => {
   return (
-
     <Provider store={store}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen name="Map" component={Map} />
+        {/* <Stack.Screen name="test" component={test} /> */}
+        <Stack.Screen name="ProfileFill" component={ProfileFill} />
+        <Stack.Screen
+          name="SuccessfullyCreated"
+          component={SuccessfullyCreated}
+        />
+
         <Stack.Screen name="MyBookMark" component={MyBookMark} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="SelectVec" component={SelectVec} />
@@ -39,24 +45,19 @@ const StackNav = () => {
           name="FillCarInformation"
           component={FillCarInformation}
         />
-
-
         <Stack.Screen name="HestoryParking" component={HestoryParking} />
         <Stack.Screen name="Parkingspot" component={Parkingspot} />
-
-        <Stack.Screen name="ProfileFill" component={ProfileFill} />
         <Stack.Screen name="MarkerDetail" component={MarkerDetail} />
         <Stack.Screen name="SearchFilter" component={SearchFilter} />
         <Stack.Screen name="MyParkings" component={MyParkings} />
         <Stack.Screen name="ParkingDetail" component={ParkingDetail} />
         <Stack.Screen name="ParkingSpot_1" component={ParkingSpot_1} />
-       <Stack.Screen name="ParkingSpot_2" component={ParkingSpot_2} />
-       <Stack.Screen name="ParkingSpot_3" component={ParkingSpot_3} />
+        <Stack.Screen name="ParkingSpot_2" component={ParkingSpot_2} />
+        <Stack.Screen name="ParkingSpot_3" component={ParkingSpot_3} />
         <Stack.Screen name="EditProfile" component={EditProfile} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </Provider>
-
-  
   );
 };
 
